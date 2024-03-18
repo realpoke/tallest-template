03604a87c58f79c80af7c1587fb2845cbbaa1567
realpoke <m@rtin.win>
2024-03-18 11:48:17 +0100

    Fixed text color in dark mode

--- resources/views/components/sections/action.blade.php
+++ resources/views/components/sections/action.blade.php
@@ -1,4 +1,4 @@
-<div {{ $attributes->merge(['class' => 'md:grid md:grid-cols-3 md:gap-6']) }}>
+<div {{ $attributes->merge(['class' => 'md:grid md:grid-cols-3 md:gap-6 text-gray-600 dark:text-gray-400']) }}>
     <x-sections.title>
         <x-slot name="title">{{ $title }}</x-slot>
         <x-slot name="description">{{ $description }}</x-slot>
