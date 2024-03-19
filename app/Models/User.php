8231118590ed7a6e6e9b219552f284177832702d
MartinT <m@rtin.win>
2024-03-19 11:25:14 +0100

    Setup roles and permissions

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,10 +9,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, HasRoles, Notifiable;
+
+    private static $defaultRole = 'user';
 
     /**
      * The attributes that are mass assignable.
@@ -46,6 +49,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::created(function ($user) {
+            $user->assignRole($user->defaultRole);
+        });
     }
 
     public function route(): string
